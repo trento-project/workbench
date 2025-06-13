6abe47cfa4e9215241fd99e5ed35891f349c6341
balanza <emanuele.decupis@suse.com>
2025-06-13 22:04:51 +0200

    fix logger

--- internal/support/log.go
+++ internal/support/log.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"strings"
 )
 
 func NewDefaultLogger(logLevel slog.Level) *slog.Logger {
@@ -16,8 +17,10 @@ func NewDefaultLogger(logLevel slog.Level) *slog.Logger {
 }
 
 type DefaultTextHandler struct {
-	w     io.Writer
-	level slog.Level
+	w      io.Writer
+	level  slog.Level
+	attrs  []slog.Attr
+	groups []string
 }
 
 func NewDefaultTextHandler(w io.Writer, level slog.Level) *DefaultTextHandler {
@@ -40,21 +43,56 @@ func (h *DefaultTextHandler) Handle(_ context.Context, r slog.Record) error {
 
 	// Append all key-value attributes
 	r.Attrs(func(attr slog.Attr) bool {
-		line += fmt.Sprintf(" %s=%v", attr.Key, attr.Value.Any())
+		line += formatAttr(attr, h.groups)
 		return true
 	})
+
+	// Append any default attributes
+	for _, attr := range h.attrs {
+		line += formatAttr(attr, []string{})
+	}
 
 	// Write the line
 	_, err := fmt.Fprintln(h.w, line)
 	return err
 }
 
-func (h *DefaultTextHandler) WithAttrs(_ []slog.Attr) slog.Handler {
-	// do nothing, all attributes are processed in Handle
-	return h
+func (h *DefaultTextHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	h2 := *h
+
+	// prepend to every attribute key the current group prefix
+	withPrefix := make([]slog.Attr, len(attrs))
+	for i, attr := range attrs {
+		withPrefix[i] = slog.Attr{
+			Key:   formatAttrKey(attr.Key, h.groups),
+			Value: attr.Value,
+		}
+	}
+
+	// Combine existing attributes with new ones
+	h2.attrs = make([]slog.Attr, len(h.attrs)+len(attrs))
+	h2.attrs = append(h.attrs, withPrefix...)
+	return &h2
 }
 
-func (h *DefaultTextHandler) WithGroup(_ string) slog.Handler {
-	// TODO: handle group
-	return h
+func (h *DefaultTextHandler) WithGroup(group string) slog.Handler {
+	h2 := *h
+	h2.groups = make([]string, len(h.groups)+1)
+	h2.groups = append(h.groups, group)
+	return &h2
+}
+
+func formatAttr(attr slog.Attr, groups []string) string {
+	return fmt.Sprintf(
+		" %s=%v",
+		formatAttrKey(attr.Key, groups),
+		attr.Value.Any(),
+	)
+}
+
+func formatAttrKey(key string, groups []string) string {
+	if len(groups) == 0 {
+		return key
+	}
+	return strings.Join(groups, ".") + "." + key
 }
