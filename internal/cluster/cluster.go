80029960466038592ac8d5c3e7844bec7aeab9a3
Emanuele De Cupis <emanuele.decupis@suse.com>
2025-08-04 17:33:11 +0200

    Add operator for CRM Cluster start (#36)
    * scaffold operator
    
    * add cluster id check
    
    * check initial status
    
    * commit phase
    
    * do rollback
    
    * verify success
    
    * add operator to registry
    
    * add retry helpers
    
    * implement backoff retry
    
    * add description
    
    * add idle check
    
    * remove clusterid check
    
    * rename module
    
    * fix
    
    * format
    
    * review retry
    
    * remove idle
    
    * rename module
    
    * rename
    
    * fix test

---
+++ internal/cluster/cluster.go
@@ -0,0 +1,84 @@
+package cluster
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"regexp"
+
+	"github.com/trento-project/workbench/internal/support"
+)
+
+var clusterIdlePatternCompiled = regexp.MustCompile("S_IDLE")
+
+type Cluster interface {
+	IsHostOnline(ctx context.Context) bool
+	IsIdle(ctx context.Context) (bool, error)
+	StartCluster(ctx context.Context) error
+	StopCluster(ctx context.Context) error
+}
+
+type ClusterClient struct {
+	executor support.CmdExecutor
+	logger   *slog.Logger
+}
+
+func NewDefaultClusterClient() Cluster {
+	return NewClusterClient(
+		support.CliExecutor{},
+		slog.Default(),
+	)
+}
+
+func NewClusterClient(executor support.CmdExecutor, logger *slog.Logger) Cluster {
+	return &ClusterClient{
+		executor: executor,
+		logger:   logger,
+	}
+}
+
+func (c *ClusterClient) IsHostOnline(ctx context.Context) bool {
+	output, err := c.executor.Exec(ctx, "crm", "status", "simple")
+	if err != nil {
+		return false
+	}
+
+	c.logger.Debug("CRM status output", "output", string(output))
+
+	return true
+}
+
+func (c *ClusterClient) StartCluster(ctx context.Context) error {
+	c.logger.Info("Starting CRM cluster")
+	output, err := c.executor.Exec(ctx, "crm", "cluster", "start")
+	if err != nil {
+		return fmt.Errorf("failed to start CRM cluster: %w, output: %s", err, string(output))
+	}
+
+	c.logger.Info("CRM cluster started successfully")
+	return nil
+}
+
+func (c *ClusterClient) StopCluster(ctx context.Context) error {
+	c.logger.Info("Stopping CRM cluster")
+	output, err := c.executor.Exec(ctx, "crm", "cluster", "stop")
+	if err != nil {
+		return fmt.Errorf("failed to stop CRM cluster: %w, output: %s", err, string(output))
+	}
+
+	c.logger.Info("CRM cluster stopped successfully")
+	return nil
+}
+
+func (c *ClusterClient) IsIdle(ctx context.Context) (bool, error) {
+	idleOutput, err := c.executor.Exec(ctx, "cs_clusterstate", "-i")
+	if err != nil {
+		return false, fmt.Errorf("error running cs_clusterstate: %w", err)
+	}
+
+	if !clusterIdlePatternCompiled.Match(idleOutput) {
+		return false, nil
+	}
+
+	return true, nil
+}
---
+++ internal/cluster/cluster_test.go
@@ -0,0 +1,59 @@
+package cluster_test
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/trento-project/workbench/internal/cluster"
+	"github.com/trento-project/workbench/internal/support/mocks"
+)
+
+type CrmTestSuite struct {
+	suite.Suite
+}
+
+func TestCrm(t *testing.T) {
+	suite.Run(t, new(CrmTestSuite))
+}
+
+func (suite *CrmTestSuite) TestIsIdle() {
+	ctx := context.Background()
+
+	mockExecutor := mocks.NewMockCmdExecutor(suite.T())
+	mockExecutor.On("Exec", ctx, "cs_clusterstate", "-i").Return([]byte("Cluster state: S_IDLE"), nil)
+
+	crmClient := cluster.NewClusterClient(mockExecutor, slog.Default())
+
+	isIdle, err := crmClient.IsIdle(ctx)
+	suite.NoError(err, "IsIdle should not return an error")
+	suite.True(isIdle, "Cluster should be idle")
+}
+
+func (suite *CrmTestSuite) TestIsIdleError() {
+	ctx := context.Background()
+
+	mockExecutor := mocks.NewMockCmdExecutor(suite.T())
+	mockExecutor.On("Exec", ctx, "cs_clusterstate", "-i").Return([]byte(""), errors.New("command failed"))
+
+	crmClient := cluster.NewClusterClient(mockExecutor, slog.Default())
+
+	_, err := crmClient.IsIdle(ctx)
+
+	suite.Error(err, "IsIdle should return an error")
+}
+
+func (suite *CrmTestSuite) TestIsIdleDifferentState() {
+	ctx := context.Background()
+
+	mockExecutor := mocks.NewMockCmdExecutor(suite.T())
+	mockExecutor.On("Exec", ctx, "cs_clusterstate", "-i").Return([]byte("Cluster state: S_TRANSITION_ENGINE"), nil)
+
+	crmClient := cluster.NewClusterClient(mockExecutor, slog.Default())
+
+	isIdle, err := crmClient.IsIdle(ctx)
+	suite.NoError(err, "IsIdle should not return an error")
+	suite.False(isIdle, "Cluster should not be idle")
+}
