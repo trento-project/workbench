053b247de04c4720d0d021072a0a05f6a09cc84d
balanza <emanuele.decupis@suse.com>
2025-08-26 12:21:22 +0200

    fix: (testpackage)

--- pkg/operator/executor_test.go
+++ pkg/operator/executor_test.go
@@ -1,4 +1,4 @@
-package operator
+package operator_test
 
 import (
 	"context"
@@ -7,11 +7,12 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	operator "github.com/trento-project/workbench/pkg/operator"
 )
 
 func TestExecutorHappyFlow(t *testing.T) {
 	executionContext := context.Background()
-	phaser := NewMockphaser(t)
+	phaser := operator.NewMockphaser(t)
 	emptyDiff := make(map[string]any)
 
 	planCall := phaser.On("plan", executionContext).
@@ -34,19 +35,19 @@ func TestExecutorHappyFlow(t *testing.T) {
 		Once().
 		NotBefore(operationDiffCall)
 
-	executor := NewExecutor(phaser, "operation-id", slog.Default())
+	executor := operator.NewExecutor(phaser, "operation-id", slog.Default())
 
 	report := executor.Run(executionContext)
 
 	assert.Equal(t, "operation-id", report.OperationID)
-	assert.Equal(t, VERIFY, report.Success.LastPhase)
+	assert.Equal(t, operator.VERIFY, report.Success.LastPhase)
 	assert.Equal(t, emptyDiff, report.Success.Diff)
 	assert.Nil(t, report.Error)
 }
 
 func TestExecutorPlanError(t *testing.T) {
 	executionContext := context.Background()
-	phaser := NewMockphaser(t)
+	phaser := operator.NewMockphaser(t)
 	planError := errors.New("error during plan phase")
 
 	phaser.On("plan", executionContext).
@@ -54,18 +55,18 @@ func TestExecutorPlanError(t *testing.T) {
 
 	phaser.AssertNotCalled(t, "after", executionContext)
 
-	executor := NewExecutor(phaser, "operation-id", slog.Default())
+	executor := operator.NewExecutor(phaser, "operation-id", slog.Default())
 
 	report := executor.Run(executionContext)
 
 	assert.Equal(t, planError.Error(), report.Error.Message)
-	assert.Equal(t, PLAN, report.Error.ErrorPhase)
+	assert.Equal(t, operator.PLAN, report.Error.ErrorPhase)
 	assert.Nil(t, report.Success)
 }
 
 func TestExecutorPlanAlreadyApplied(t *testing.T) {
 	executionContext := context.Background()
-	phaser := NewMockphaser(t)
+	phaser := operator.NewMockphaser(t)
 	emptyDiff := make(map[string]any)
 
 	planCall := phaser.On("plan", executionContext).
@@ -80,19 +81,19 @@ func TestExecutorPlanAlreadyApplied(t *testing.T) {
 		Once().
 		NotBefore(operationDiffCall)
 
-	executor := NewExecutor(phaser, "operation-id", slog.Default())
+	executor := operator.NewExecutor(phaser, "operation-id", slog.Default())
 
 	report := executor.Run(executionContext)
 
 	assert.Equal(t, "operation-id", report.OperationID)
-	assert.Equal(t, PLAN, report.Success.LastPhase)
+	assert.Equal(t, operator.PLAN, report.Success.LastPhase)
 	assert.Equal(t, emptyDiff, report.Success.Diff)
 	assert.Nil(t, report.Error)
 }
 
 func TestExecutorCommitErrorWithSuccessfulRollback(t *testing.T) {
 	executionContext := context.Background()
-	phaser := NewMockphaser(t)
+	phaser := operator.NewMockphaser(t)
 	commitError := errors.New("error during error phase")
 
 	planCall := phaser.On("plan", executionContext).
@@ -111,18 +112,18 @@ func TestExecutorCommitErrorWithSuccessfulRollback(t *testing.T) {
 		Once().
 		NotBefore(rollbackCall)
 
-	executor := NewExecutor(phaser, "operation-id", slog.Default())
+	executor := operator.NewExecutor(phaser, "operation-id", slog.Default())
 
 	report := executor.Run(executionContext)
 
 	assert.Equal(t, commitError.Error(), report.Error.Message)
-	assert.Equal(t, COMMIT, report.Error.ErrorPhase)
+	assert.Equal(t, operator.COMMIT, report.Error.ErrorPhase)
 	assert.Nil(t, report.Success)
 }
 
 func TestExecutorCommitErrorWithFailedRollback(t *testing.T) {
 	executionContext := context.Background()
-	phaser := NewMockphaser(t)
+	phaser := operator.NewMockphaser(t)
 	commitError := errors.New("error during error phase")
 	rollbackError := errors.New("error during rollback phase")
 
@@ -142,18 +143,18 @@ func TestExecutorCommitErrorWithFailedRollback(t *testing.T) {
 		Once().
 		NotBefore(rollbackCall)
 
-	executor := NewExecutor(phaser, "operation-id", slog.Default())
+	executor := operator.NewExecutor(phaser, "operation-id", slog.Default())
 
 	report := executor.Run(executionContext)
 
 	assert.Equal(t, errors.Join(rollbackError, commitError).Error(), report.Error.Message)
-	assert.Equal(t, ROLLBACK, report.Error.ErrorPhase)
+	assert.Equal(t, operator.ROLLBACK, report.Error.ErrorPhase)
 	assert.Nil(t, report.Success)
 }
 
 func TestExecutorVerifyErrorWithSuccessfulRollback(t *testing.T) {
 	executionContext := context.Background()
-	phaser := NewMockphaser(t)
+	phaser := operator.NewMockphaser(t)
 	verifyError := errors.New("error during verify phase")
 
 	planCall := phaser.On("plan", executionContext).
@@ -176,18 +177,18 @@ func TestExecutorVerifyErrorWithSuccessfulRollback(t *testing.T) {
 		Once().
 		NotBefore(rollbackCall)
 
-	executor := NewExecutor(phaser, "operation-id", slog.Default())
+	executor := operator.NewExecutor(phaser, "operation-id", slog.Default())
 
 	report := executor.Run(executionContext)
 
 	assert.Equal(t, verifyError.Error(), report.Error.Message)
-	assert.Equal(t, VERIFY, report.Error.ErrorPhase)
+	assert.Equal(t, operator.VERIFY, report.Error.ErrorPhase)
 	assert.Nil(t, report.Success)
 }
 
 func TestExecutorVerifyErrorWithFailedRollback(t *testing.T) {
 	executionContext := context.Background()
-	phaser := NewMockphaser(t)
+	phaser := operator.NewMockphaser(t)
 	verifyError := errors.New("error during verify phase")
 	rollbackError := errors.New("error during rollback phase")
 
@@ -211,11 +212,11 @@ func TestExecutorVerifyErrorWithFailedRollback(t *testing.T) {
 		Once().
 		NotBefore(rollbackCall)
 
-	executor := NewExecutor(phaser, "operation-id", slog.Default())
+	executor := operator.NewExecutor(phaser, "operation-id", slog.Default())
 
 	report := executor.Run(executionContext)
 
 	assert.Equal(t, errors.Join(rollbackError, verifyError).Error(), report.Error.Message)
-	assert.Equal(t, ROLLBACK, report.Error.ErrorPhase)
+	assert.Equal(t, operator.ROLLBACK, report.Error.ErrorPhase)
 	assert.Nil(t, report.Success)
 }
